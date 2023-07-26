3e4164b96cd1380fd71a7b526c590622ddeb51ab
Ziv Attias <ziv.attias7@gmail.com>
2023-07-26 13:22:40 +0300

    add .env logic to main.go

--- src/main.go
+++ src/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbum)
 	router.GET("/albums/:id", getAlbumById)
+	router.POST("/albums/send", sendAlbumsByEmail)
 
 	router.Run("localhost:8080")
 }
